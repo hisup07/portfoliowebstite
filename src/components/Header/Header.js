5551ab9f05281884159bc2462313b72ddd07f7e0
supriyomondal <hisupriyooo@gmail.com>
2021-12-29 10:58:01 +0530

    complete header ui

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,66 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillFacebook, AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+  Span,
+} from './HeaderStyles';
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href='/'>
+        <a
+          href=''
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            color: 'white',
+            marginBottom: '20px',
+          }}
+        >
+          <DiCssdeck size='3rem' />
+          <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href='#projects'>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#tech'>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#about'>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    {/*  TODO : change links */}
+    <Div3>
+      <SocialIcons href='https://github.com'>
+        <AiFillGithub size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://linkedin.com'>
+        <AiFillLinkedin size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://insta.com'>
+        <AiFillFacebook size='3rem' />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
