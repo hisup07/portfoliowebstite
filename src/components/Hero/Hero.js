2fd4a24c5a70ae0130844f2b27602d5b722ff8aa
supriyomondal <hisupriyooo@gmail.com>
2021-12-29 11:07:56 +0530

    complete hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there!! <br />
+        Welcome To My Personal Portfolio.
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae aliquam
+        tempora consequuntur nam error distinctio sit sint explicabo temporibus!
+        Tempore.
+      </SectionText>
+      <Button
+        onClick={() => {
+          console.log('hello');
+        }}
+      >
+        Connect with me
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
